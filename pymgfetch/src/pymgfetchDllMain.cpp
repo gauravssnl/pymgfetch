edddab61b9c5cdb761de068d90e1038412fbca37
treitmaier <treitmaier@gmail.com>
2009-08-14 10:27:29 +0000

    Created a version for PyS60 1.9.7 called pymgfetch2. The version for PyS60 1.4.5 is called pymgfetch.

---
+++ pymgfetch/src/pymgfetchDllMain.cpp
@@ -0,0 +1,33 @@
+/*
+ ============================================================================
+ Name		: pymgfetchDll.cpp 
+ Author	  : Thomas Reitmaier
+ Copyright   : Copyright Thomas Reitmaier.
+ Description : pymgfetchDll.cpp - main DLL source
+ ============================================================================
+ */
+
+//  Include Files  
+
+#include <e32std.h>		 // GLDEF_C
+#include "pymgfetch.pan"		// panic codes
+
+//  Global Functions
+
+GLDEF_C void Panic(TpymgfetchPanic aPanic)
+// Panics the thread with given panic code
+	{
+	User::Panic(_L("pymgfetch"), aPanic);
+	}
+
+//  Exported Functions
+
+#ifndef EKA2 // for EKA1 only
+EXPORT_C TInt E32Dll(TDllReason /*aReason*/)
+// Called when the DLL is loaded and unloaded. Note: have to define
+// epoccalldllentrypoints in MMP file to get this called in THUMB.
+	{
+	return KErrNone;
+	}
+#endif
+
