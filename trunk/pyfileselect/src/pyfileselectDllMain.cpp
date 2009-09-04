af9a8e5dca6fe6d0b782f4fe97790a4ea24f88c8
treitmaier@gmail.com <treitmaier@gmail.com@631531f2-7635-11de-8480-e142f2bfdb3b>
2009-09-04 09:36:42 +0000

    Initial upload of pyfileselect for PyS60 1.4.5.

---
+++ trunk/pyfileselect/src/pyfileselectDllMain.cpp
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name		: pyfileselectDll.cpp 
+ Author	  : Thomas Reitmaier
+ Copyright   : Your copyright notice
+ Description : pyfileselectDll.cpp - main DLL source
+ ============================================================================
+ */
+
+//  Include Files  
+
+#include <e32std.h>		 // GLDEF_C
+
+#ifndef EKA2 // for EKA1 only
+EXPORT_C TInt E32Dll(TDllReason /*aReason*/)
+// Called when the DLL is loaded and unloaded. Note: have to define
+// epoccalldllentrypoints in MMP file to get this called in THUMB.
+	{
+	return KErrNone;
+	}
+#endif
+
